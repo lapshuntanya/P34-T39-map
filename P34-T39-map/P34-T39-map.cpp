9344da5bf2ef4fa4fb0b30a3034cff18ed27f6c0
lapshuntanya <lapshun.tatiana@gmail.com>
2024-12-19 16:48:37 +0200

    task1

--- P34-T39-map/P34-T39-map.cpp
+++ P34-T39-map/P34-T39-map.cpp
@@ -1,7 +1,29 @@
-
 #include <iostream>
+#include <string>
+#include <map>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	map<string, int> words;
+	string text;
+
+	do {
+		cout << "Input text: ";
+		cin >> text;
+
+		if (words.find(text) == words.end()) {
+			//words.insert(pair<string, int>(text, 1));
+			words[text] = 1;
+		}
+		else {
+			words[text]++;
+		}
+	} while (text != "stop");
+	cout << "----------------------------------------------\n";
+
+	words.erase("stop");
+	for (auto item = words.begin(); item != words.end(); item++) {
+		cout << item->first << " " << item->second << endl;
+	}
 }
